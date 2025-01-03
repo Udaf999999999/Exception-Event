11cac2a85e718dce66364bcc555d13ab390d899e
Udaf999999999 <udaf999999999@yandex.ru>
2025-01-03 21:47:30 +0700

    Add Solution

---
+++ Exception-EventConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exception_EventConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
