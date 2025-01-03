8656b68f8d2b8662be514de1be86155626f46a05
Udaf999999999 <udaf999999999@yandex.ru>
2025-01-03 22:27:46 +0700

    Add MyException and FileScraping class

---
+++ Exception-EventConsoleApp/FileScraping.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_EventConsoleApp
+{
+    internal class FileScraping
+    {
+        private FileInfo file;
+        public FileScraping(string filePath)
+        {
+            //Exception[] exceptions = new Exception[]
+            //{
+            //    new MyException("MyException!"),
+            //    new ArgumentNullException("Path's string is null!"),
+            //    new DriveNotFoundException("Drive isn't accepteble or found!"),
+            //    new FileNotFoundException("File not found!"),
+            //    new PathTooLongException("Path is too long!")
+            //};
+            Exception[] exceptions = {System.AggregateException };
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                //foreach (var ex2 in exceptions)
+                //{
+                //    if (ex is ex2)
+                //    {
+                //        Console.WriteLine(ex2.Message);
+                //    }
+                //}
+            }
+        }
+        public void Scraping()
+        {
+
+        }
+    }
+}
---
+++ Exception-EventConsoleApp/MyException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_EventConsoleApp
+{
+    internal class MyException : Exception
+    {
+        public MyException()
+        {
+            Console.WriteLine("My Exception happened:" + DateTime.Now);
+        }
+
+        public MyException(string message) : base(message)
+        {
+            Console.WriteLine("My Exception happened:" + DateTime.Now);
+        }
+    }
+}
