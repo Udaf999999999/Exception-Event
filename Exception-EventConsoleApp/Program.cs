e935c275b034374649e0c03e24690d3a41f69f89
Udaf999999999 <udaf999999999@yandex.ru>
2025-02-05 17:16:19 +0700

    Change programm.cs add comments

--- Exception-EventConsoleApp/Program.cs
+++ Exception-EventConsoleApp/Program.cs
@@ -39,7 +39,7 @@
             //surnameSortThrowEventList.Sort();
             //surnameSortThrowEventList.ShowSurnames();
 
-            FileScraping fileScraping = new FileScraping(@"C:\Windows\System32\drivers\etc\hosts", "localhostn");
+            //FileScraping fileScraping = new FileScraping(@"C:\Windows\System32\drivers\etc\hosts", "localhost");
         }
     }
 }
